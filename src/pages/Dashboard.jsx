3356cbe847073b232ba506986a6b886f7e1875d7
Valerie Samuels <163075955+vsamuels213@users.noreply.github.com>
2026-04-01 18:21:47 -0400

    remove test button

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -25,12 +25,6 @@ function Dashboard() {
       <div className="bg-white rounded-lg shadow p-8 text-center">
         <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
         <p className="text-gray-600 mb-6">Logged in as <span className="font-medium">{email}</span></p>
-        
-        <button
-        className="px-4 py-2 bg-yellow-200 hover:bg-purple-800">
-        Send help
-        </button>
-        <br></br>
         <button
           onClick={handleSignOut}
           className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600"
