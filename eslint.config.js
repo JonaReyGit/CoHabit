42127f395dcbf2a4ac6e52780f713ee6fd27657c
Valerie Samuels <163075955+vsamuels213@users.noreply.github.com>
2026-04-14 23:26:03 -0400

    added config file for static code analysis

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    ignores: ["src/components/ui/**"],
+  },
+
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+
+  {
+    files: ["**/*.{js,jsx}"],
+    ...pluginReact.configs.flat.recommended,
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "no-unused-vars": ["error", { "varsIgnorePattern": "^[A-Z]" }],
+    },
+  },
+]);
